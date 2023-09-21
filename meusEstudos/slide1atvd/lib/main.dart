016d76c65fe0145b85e2936acbe4b8ce827f35b4
FaustinoEdu <kingdosemdermam@gmail.com>
2023-09-20 23:11:09 -0400

    Atividade Slide 1

---
+++ meusEstudos/slide1atvd/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main(){
+runApp(
+const Center(
+child: Text(
+"Fala comigo bebê",
+textDirection: TextDirection.ltr ,
+style: TextStyle(
+fontFamily: "ArialBlack",
+fontSize: 30.0,
+
+
+),
+
+
+
+
+),
+
+
+
+
+
+),
+
+);
+
+}
