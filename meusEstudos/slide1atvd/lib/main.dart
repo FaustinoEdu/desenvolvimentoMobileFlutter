c8c6edbb279b373eb2eeb8cda724ba6ed95979c4
FaustinoEdu <kingdosemdermam@gmail.com>
2023-09-22 11:44:42 -0400

    Adicionando uma childreeen e fazendo o enunciado

--- meusEstudos/slide1atvd/lib/main.dart
+++ meusEstudos/slide1atvd/lib/main.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp(
-const Center(
-child: Text(
-"Fala comigo bebê",
-textDirection: TextDirection.ltr ,
-style: TextStyle(
-fontFamily: "ArialBlack",
-fontSize: 30.0,
+void main() => runApp(const MyApp());
 
-
-),
-
-
-
-
-),
-
-
-
-
-
-),
-
-);
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: 
+        Column(children: [
+          Container(
+           
+            child: Text(
+              "Faustino José Linhares Jr",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
 }
